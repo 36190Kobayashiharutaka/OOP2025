7cfa66f2f6e5f8c6756a28319ef5c268468555f9
infosys <infosys@C4212>
2025-04-21 09:57:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karintou = new Product(123, "かりんとう", 180);
+            Product daihuku = new Product(250, "daihuku", 150);
+            //かりんとう
+
+            //税抜きの価格を表示【かりんとうの税抜き価格は○○円です】
+
+            Console.WriteLine(karintou.Name + "税抜きの価格は" + karintou.Price + "円です");
+
+            //消費税額の表示【かりんとうの消費税額は○○円です】
+
+            Console.WriteLine(karintou.Name + "消費税額は" + karintou.GetTax() + "円です");
+
+            //税込み価格の表示【かりんとうの税込み価格は○○円です】
+            Console.WriteLine(karintou.Name + "税込み価格は" + karintou.GetPriceIncludingTax() + "円です");
+
+            //大福
+
+            //税抜きの価格を表示【大福の税抜き価格は○○円です】
+
+            Console.WriteLine(daihuku.Name + "税抜きの価格は" + daihuku.Price + "円です");
+
+            //消費税額の表示【大福の消費税額は○○円です】
+
+            Console.WriteLine(daihuku.Name + "消費税額は" + daihuku.GetTax() + "円です");
+
+            //税込み価格の表示【大福の税込み価格は○○円です】
+            Console.WriteLine(daihuku.Name + "税込み価格は" + daihuku.GetPriceIncludingTax() + "円です");
+
+        }
+    }
+}
---
+++ Chapter02/DistamceConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DistamceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //フィートからメートルへの対応表を表示
+            int f = int.Parse(args[1]);
+            int e = int.Parse(args[2]);
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeterList(f, e);
+            } else {
+                PrintFeetToMeterList(f, e);
+            }
+            //フィートからメートルへの対応表を出力
+                static void PrintFeetToMeterList(int f, int e) {
+                    for (int feet = f; feet <= e; feet++) {
+                        double meter = FeetToMeter(feet);
+                        Console.WriteLine($"{feet}ft = {meter:0.0000}");
+                    }
+                }
+            //フィートからメートルへの対応表を出力
+            static void PrintMeterToFeetList(int f, int e) {
+                for (int meter = f; meter <= e; meter++) {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+                }
+            }
+
+
+            static double FeetToMeter(int feet) {
+                return feet * 0.3048;
+            }
+            static double MeterToFeet(int meter) {
+                return meter / 0.3048;
+
+            }
+        }
+    }
+}
