ef650ecd9726b895ab72feffc0d6dedf9d9a765d
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-04-23 09:31:46 +0900

    インチからメートル変換完成

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercise02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        for (int inch = 1; inch <= 10; inch++) {
+            double meter = inch * 0.0254;
+            Console.WriteLine($"{inch}ih = {meter:0.0000}m");
+
+        }
+    }
+}
