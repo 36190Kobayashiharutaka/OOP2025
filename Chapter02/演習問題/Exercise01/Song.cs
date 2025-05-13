4345b33b4bc2cfbb8f507b1a96f701ea7d88e7f5
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-05-13 10:31:02 +0900

    2.1.1

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+    public class Song {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        // コンストラクター
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+
+        public override string ToString() {
+            return $"Title: {Title}, Artist: {ArtistName}, Length: {Length}秒";
+        }
+
+
+    }
+}
