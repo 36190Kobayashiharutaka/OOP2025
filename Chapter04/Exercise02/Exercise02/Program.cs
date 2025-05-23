2d929a9096d29832cc5e1880bf5d34591aae0f2d
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-05-23 15:24:16 +0900

    4.2 わからなかった

---
+++ Chapter04/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            var line = Console.ReadLine();
+
+        }
+
+        private static void Exercise2() {
+            
+        }
+
+        private static void Exercise3() {
+            
+        }
+    }
+}
