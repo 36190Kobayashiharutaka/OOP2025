5385932b26f356522f48a90622f17e9cb2e458da
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-05-23 15:07:08 +0900

    4.1完成

--- Chapter04/Exercise01/Program.cs
+++ Chapter04/Exercise01/Program.cs
@@ -15,15 +15,48 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            //foreach
+            foreach (string lang in langs) {
+                if (lang.Contains("S")) {
+                    Console.WriteLine(lang);
+                }
+            }
+            //for
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains("S")) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+            //While
+            int j = 0;
+            while (j < langs.Count) {
+                if (langs[j].Contains("S")) {
+                    Console.WriteLine(langs[j]);
+                }
+                j++;
+            }
         }
 
+
         private static void Exercise2(List<string> langs) {
-            
+            IEnumerable<string> Langs = langs.Where(lang => lang.Contains("S"));
+
+            foreach (string lang in Langs) {
+                Console.WriteLine(lang);
+            }
+
+            var LangsQuery = from lang in langs
+                                     where lang.Contains("S")
+                                     select lang;
+
+            foreach (string lang in LangsQuery) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-
+            string lang = langs.Find(l => l.Length >= 10);
+            lang = lang ?? "unknown";
         }
     }
 }
