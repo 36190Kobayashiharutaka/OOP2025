6421798c817551d0048ff5d28586523d433d1842
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-05-28 10:57:04 +0900

    7.2.1～7.2.2まで完成

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks();
+
+
+            //本の平均金額を表示
+
+            Console.WriteLine(books.Average(b => b.Price));
+
+            //本のページ合計を表示
+
+            Console.WriteLine(Books.GetBooks().Sum(book => book.Pages));
+
+            //金額の安い書籍名と金額を表示
+
+            var book = books.Where(x => x.Price == books.Min(b => b.Price));
+            foreach(var item in book) {
+                Console.WriteLine(item.Title + " : " + item.Price);
+            }
+
+            //ページが多い書籍名とページ数を表示
+
+            books.Where(x => x.Pages == books.Max(b => b.Pages)).ToList().ForEach(x => Console.WriteLine($"{x.Title} : {x.Pages}ページ"));
+
+
+
+            //タイトルに「物語」が含まれている書籍名をすべて表示
+            Books.GetBooks().Where(book => book.Title.Contains("物語")).ToList().ForEach(book => Console.WriteLine(book.Title));
+
+
+        }
+    }
+}
---
+++ Chapter07/Section05/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var text = "The quick brown fox jumps over the lazy dog";
+            var words = text.Split(' ');
+            var ward = words.Where(s => s.Length == 4);
+            
+
+
+            var numbres = new List<int> { 9, 6, -5, -4, 2, 5, -4, 8 - 1, 0, 4 };
+            var index = numbres.FindIndex(n => n < 0);
+            
+
+        }
+    }
+}
