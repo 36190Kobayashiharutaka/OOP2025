a69acbe31d5cded8fbb6251747d4018c102eb0fa
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-06-10 09:47:33 +0900

    5.1.3回答

---
+++ Chapter05/Section01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var obj = new MySample;
+
+            ValueTask memList = obj.MyList(6).RemoveAt(0);
+        }
+    }
+
+    class MySample {
+        public ImmutableList<int> MyList { get; private set; }
+
+        public MySample() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList = list.ToImmutableList();
+        }
+    }
+    public class Person {
+        public string GivenName { get; private set; }
+
+        public string FamilyName { get; private set; }
+
+        public Person(string familyName, string givenName) {
+            FamilyName = familyName;
+            GivenName = givenName;
+        }
+
+      //5.1.2
+      //設定されている西暦が21世紀か判定する
+      //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
+      public bool
+            
+            
+            
+            
+          
+    }
+}
