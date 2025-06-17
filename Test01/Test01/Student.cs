c356e7418d60a53b3eb79fe3d1c70a52593adfee
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-06-17 11:12:05 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name {private get; set; }
+        public string Subject { private get; set; }
+        public int Score { private get; set; }
+
+
+
+
+    }
+}
