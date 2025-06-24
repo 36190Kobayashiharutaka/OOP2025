912dd0eeda80ad347b6f8a96fb30f1cd9c5134d7
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-06-24 15:05:53 +0900

    dauofweek

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            // var today = new DateTime(2025,7,12);//日付
+            //var now = DateTime.Now;//日付と時刻
+
+
+
+            //Console.WriteLine($"Today:{today.Month}");
+            //Console.WriteLine($"Now:{now}");
+
+
+            //自分生年月日は何曜日かをプログラムを書いて調べる
+ 
+            Console.WriteLine("西暦：");
+            var year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("月：");
+            var month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("日：");
+            var day = int.Parse(Console.ReadLine());
+
+            var birth = new DateTime(year, month, day);
+
+
+
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var shortDayOfWeek = culture.DateTimeFormat.GetShortestDayName(birth.DayOfWeek);
+
+
+
+            
+        }
+    }
+}
