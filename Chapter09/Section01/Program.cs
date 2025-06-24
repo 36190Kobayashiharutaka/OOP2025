093d35ea6817bef7da47057e79c8d08406e91cdf
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-06-24 16:00:00 +0900

    6/24

--- Chapter09/Section01/Program.cs
+++ Chapter09/Section01/Program.cs
@@ -29,10 +29,31 @@ namespace Section01 {
 
             var culture = new CultureInfo("ja-JP");
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            var str = birth.ToString("ggyy年M月d日", culture);
             var shortDayOfWeek = culture.DateTimeFormat.GetShortestDayName(birth.DayOfWeek);
+            
+            Console.WriteLine(str + birth.ToString("ddd曜日", culture));
 
+            //生まれてから○○○○日目です。
+            TimeSpan diff;
+            while (true) {
+                diff = DateTime.Now - birth;
+                Console.WriteLine($"r{diff.TotalDays}日 {diff.TotalHours}時間 {diff.TotalMinutes}分");//生まれてからの経過秒数
 
+            }
+            //あなたは○○歳です！
+            int age = GatAge(birth, DateTime.Today);
+            Console.WriteLine(age + "歳");
+            //一月一日から何日目か？
 
+            static int GatAge(DateTime brithday,DateTime targetDay) {
+                var age = targetDay.Year - brithday.Year;
+                if (targetDay < brithday.AddYears(age)) {
+                    age--;
+                }
+                return age;
+            }
             
         }
     }
