8aef1f7dc971f4750a2554d923ed5bead6892e33
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-10-30 14:59:34 +0900

    Code13_7

---
+++ Chapter14/Section03/Form1.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Section03 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "";
+            var elapsed = await Task.Run(()=>DoLongTimeWork());
+            toolStripStatusLabel1.Text = "終了";
+        }
+        private long DoLongTimeWork() {
+            // 長時間かかる処理をシミュレート
+            var sw = Stopwatch.StartNew();
+
+            System.Threading.Thread.Sleep(5000); // 5秒待機
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
