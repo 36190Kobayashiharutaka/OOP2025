722b344b78f49cf4f43ded5bc2855462efaca97f
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-11-05 11:16:48 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+            await GetHtmExample(hc);
+
+        }
+
+        static async Task GetHtmExample(HttpClient httpClient) {
+            var url = "https://www.google.com/?hl=ja";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+
+    }
+}
