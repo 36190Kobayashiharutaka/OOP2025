9d0341013bf63fa50512bb29beabf5b4bf85e084
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-11-06 14:42:03 +0900

    WebBrowser

---
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        InitialzeAsync();
+    }
+
+    private async void InitialzeAsync() {
+        await WebView.EnsureCoreWebView2Async();//非同期にしてブラウザの初期化処理をしている
+
+        WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+    }
+
+
+    //読み込みが開始したらプログレスバーを表示
+    private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+        LoadingBar.Visibility = Visibility.Visible;
+        LoadingBar.IsIndeterminate = true;
+    }
+
+    //読み込みが完了したらプログレスバーを非表示
+    private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+        LoadingBar.Visibility = Visibility.Collapsed;
+        LoadingBar.IsIndeterminate = false;
+    }
+
+
+    private void BackButton_Click(object sender, RoutedEventArgs e) {
+        // WebView2 に戻る履歴があるかチェック
+        if (WebView.CanGoBack) {
+            WebView.GoBack();
+        }
+    }
+
+    private void FowardButton_Click(object sender, RoutedEventArgs e) {
+        // WebView2 に進む履歴があるかチェック
+        if (WebView.CanGoForward) {
+            WebView.GoForward();
+        }
+    }
+
+    private void GoButton_Click(object sender, RoutedEventArgs e) {
+        //WebView.Source = new Uri(AddressBar.Text);
+        var url = AddressBar.Text.Trim();
+
+        if (string.IsNullOrWhiteSpace(url)) return;
+
+        WebView.Source = new Uri(url);
+    }
+}
