b89e85d530c574662b4c126d81bbfb075e75a821
Kobayashiharutaka <oibc36190@ojs.ac.jp>
2025-11-19 10:33:17 +0900

    問15.1修正

--- Chapter15/TemplateMethod/TextFileProcessor/TextFileProcessorDI/LineToHalfNumberService.cs
+++ Chapter15/TemplateMethod/TextFileProcessor/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace TextFileProcessorDI {
     public class LineToHalfNumberService : ITextFileService {
         public void Execute(string line) {
-
-        }
-        public void Initialize(string fname) {
             string result = new string(
                 line.Select(c => ('０' <= c && c <= '９') ? (char)(c - '０' + '0') : c).ToArray());
             Console.WriteLine(result);
+        }
+
+        public void Initialize(string fname) {
         }
         public void Terminate() {
 
